9f15d6f10ba2cff882a1a4059a43894bc66c2ebf
Caio Queiroz <caio.softbuilder>
2021-11-25 01:32:29 -0300

    rota todo para criação de todo

--- src/index.js
+++ src/index.js
@@ -12,18 +12,25 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   const {username} = request.headers;
+  const {url, method} = request;
 
   const userAlreadyExist = users.find(user => user.username === username);
 
-  if(userAlreadyExist){
+  const regexTodos = new RegExp(/\/todos/gi);
+
+  if(userAlreadyExist && url === '/users' && method === 'POST'){
     return response.status(401).json({error: "Username já existente"});
   }
+
+  if(url.match(regexTodos)?.length > 0 && !userAlreadyExist){
+    return response.status(404).json({error: "Usuário inexistente!"});
+  }
+
+  request.user = userAlreadyExist;
   next();
 }
 
-app.use(checksExistsUserAccount);
-
-app.post('/users', (request, response) => {
+app.post('/users', checksExistsUserAccount, (request, response) => {
   const {name, username} = request.body;
 
   const user = {
@@ -35,15 +42,29 @@ app.post('/users', (request, response) => {
 
   users.push(user);
 
-  return response.status(201).json({data: user});
+  return response.status(201).json({user: user});
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  return response.status(200).json({todos: user.todos});
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: Date.now()
+  }
+
+  user.todos.push(todo);
+  users[users.indexOf(user)] = user;
+  return response.status(201).json({user: user});
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
